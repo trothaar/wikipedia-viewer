0ff5c41cda3701bd2651ad92c16e9514f30d57a7
Teresa <trothaar@users.noreply.github.com>
2017-01-18 19:17:27 -0500

    Add files via upload

---
+++ wikiViewer.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+
+$(".searchbox").autocomplete({
+    source: function(request, response) {
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php",
+            dataType: "jsonp",
+            data: {
+                'action': "opensearch",
+                'format': "json",
+                'search': request.term
+            },
+            success: function(data) {
+                response(data[1]);
+            }
+        });
+    }
+});
+});
